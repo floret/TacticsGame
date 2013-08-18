631b635420b7cd2205edb4dbec0ab7bf1e6519b4
floret <sorrowbedamned@gmail.com>
2013-08-18 18:21:12 +0200

    Changed size of game board

--- TacticsGame/Form1.Designer.cs
+++ TacticsGame/Form1.Designer.cs
@@ -28,14 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlGameBoard = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // pnlGameBoard
+            // 
+            this.pnlGameBoard.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlGameBoard.Location = new System.Drawing.Point(156, 149);
+            this.pnlGameBoard.Name = "pnlGameBoard";
+            this.pnlGameBoard.Size = new System.Drawing.Size(800, 450);
+            this.pnlGameBoard.TabIndex = 0;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(1000, 650);
+            this.ClientSize = new System.Drawing.Size(1137, 750);
+            this.Controls.Add(this.pnlGameBoard);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -45,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlGameBoard;
     }
 }
 
